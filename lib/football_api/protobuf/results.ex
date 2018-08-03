e3e507198161800a44ce6adc5ceb15c70c0426dc
afatsini <fatsifont2@gmail.com>
2018-08-03 17:13:39 +0200

    Move controller code to business logic

--- lib/football_api/protobuf/results.ex
+++ lib/football_api/protobuf/results.ex
@@ -1,3 +1,16 @@
 defmodule FootballApi.Protobuf.Results do
   use Protobuf, from: Path.expand("result.proto", __DIR__)
+
+  alias FootballApi.Protobuf.Results.Result
+
+  def to_proto(entry) when is_map(entry) do
+    entry
+    |> Result.new()
+    |> Result.encode()
+  end
+
+  def to_proto(entries) when is_list(entries) do
+    entries
+    |> Enum.map(fn result -> to_proto(result) end)
+  end
 end
--- lib/football_api_web/controllers/v1/protobuffer/results_controller.ex
+++ lib/football_api_web/controllers/v1/protobuffer/results_controller.ex
@@ -3,12 +3,12 @@ defmodule FootballApiWeb.V1.Protobuffer.ResultsController do
   use FootballApiWeb, :controller
 
   alias FootballApi.DataServer
-  alias FootballApi.Protobuf.Results.Result
+  alias FootballApi.Protobuf.Results
 
   def index(conn, _params) do
     results =
       DataServer.get_by([])
-      |> Enum.map(fn result -> result |> Result.new() |> Result.encode() end)
+      |> Results.to_proto()
 
     conn
     |> put_resp_header("content-type", "application/x-protobuf")
