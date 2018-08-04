95b9bf9607bf8ecf17082360a3c3263bb417ff15
afatsini <fatsifont2@gmail.com>
2018-08-04 20:04:24 +0200

    default error view

--- lib/football_api_web/views/error_view.ex
+++ lib/football_api_web/views/error_view.ex
@@ -1,6 +1,18 @@
 defmodule FootballApiWeb.ErrorView do
   use FootballApiWeb, :view
 
+  def render("404.html", _assigns) do
+    "Not found"
+  end
+
+  def render("400.html", _assigns) do
+    "Bad Request"
+  end
+
+  def render(<<_status::binary-size(3)>> <> ".html", _assigns) do
+    "Internal Server Error"
+  end
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.json", _assigns) do
