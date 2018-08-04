985f96bac5d4c743c51051898ee63f3fec350d02
afatsini <fatsifont2@gmail.com>
2018-08-04 12:24:59 +0200

    refactor

--- lib/football_api_web/json_controller.ex
+++ lib/football_api_web/json_controller.ex
@@ -12,18 +12,8 @@ defmodule FootballApiWeb.JsonController do
   def init(default), do: default
 
   @spec call(Conn.t(), any) :: Conn.t()
-  def call(conn, {:error, :bad_request}), do: bad_input(conn)
-  def call(conn, {:error, :not_found}), do: not_found(conn)
-
-  @spec bad_input(Conn.t()) :: Conn.t()
-  def bad_input(conn), do: do_error(conn, 400)
-
-  @spec not_found(Conn.t()) :: Conn.t()
-  def not_found(conn), do: do_error(conn, 404)
-
-  @spec unprocessable_entity(Conn.t()) :: Conn.t()
-  def unprocessable_entity(conn),
-    do: do_error(conn, 422)
+  def call(conn, {:error, :bad_request}), do: do_error(conn, 400)
+  def call(conn, {:error, :not_found}), do: do_error(conn, 404)
 
   @spec do_error(Conn.t(), http_code :: pos_integer) :: Conn.t()
   defp do_error(conn, http_code) do
--- lib/football_api_web/proto_controller.ex
+++ lib/football_api_web/proto_controller.ex
@@ -9,24 +9,10 @@ defmodule FootballApiWeb.ProtoController do
   def init(default), do: default
 
   @spec call(Conn.t(), any) :: Conn.t()
-  def call(conn, {:error, :bad_request}), do: bad_input(conn)
-  def call(conn, {:error, :not_found}), do: not_found(conn)
-  def call(conn, error), do: server_error(conn, error)
+  def call(conn, {:error, :bad_request}), do: do_error(conn, 400, "Bad Request")
+  def call(conn, {:error, :not_found}), do: do_error(conn, 404, "Not found")
 
   @typep reason :: any
-
-  @spec bad_input(Conn.t(), reason) :: Conn.t()
-  def bad_input(conn, reason \\ "Bad Request"), do: do_error(conn, 400, reason)
-
-  @spec not_found(Conn.t(), reason) :: Conn.t()
-  def not_found(conn, reason \\ "Not found"), do: do_error(conn, 404, reason)
-
-  @spec unprocessable_entity(Conn.t(), reason) :: Conn.t()
-  def unprocessable_entity(conn, reason \\ "Unprocessable entity"),
-    do: do_error(conn, 422, reason)
-
-  @spec server_error(Conn.t(), reason) :: Conn.t()
-  def server_error(conn, reason), do: do_error(conn, 500, reason)
 
   @spec do_error(Conn.t(), http_code :: pos_integer, reason) :: Conn.t()
   defp do_error(conn, http_code, reason) do
